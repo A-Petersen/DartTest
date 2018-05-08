a388cae95e63f23e94449b596e3a0c8341a77305
DanielFGA <DanielFGA>
2018-05-08 11:35:26 +0200

    Kann man mit arbeiten

--- web/main.dart
+++ web/main.dart
@@ -1,50 +1,23 @@
+import 'model/Fruit.dart';
+import 'model/Field.dart';
 import 'dart:html';
 import 'dart:async';
 import 'dart:math';
 
-class Field {
-  TableRowElement row;
-  TableCellElement cell;
-  int rowValue;
-  int cellValue;
-
-  Field(TableRowElement row, TableCellElement cell, int rowValue, cellValue) {
-    this.row = row;
-    this.cell = cell;
-    this.rowValue = rowValue;
-    this.cellValue = cellValue;
-  }
-
-  TableCellElement get getCell => cell;
-
-
-}
-
-List tableFields = new List();
+final field = new Field();
+Fruit fruit = new Fruit(field.center_x, field.center_y, field.size / 4, field);
 
 void main() {
 
-  TableElement table = document.getElementById("table");
-  tableFields = new List();
-
-  for (int i = 0 ; i < 8 ; i++) {
-    TableRowElement row = table.addRow();
-    for (int j = 0 ; j < 8 ; j++) {
-      TableCellElement cell = row.addCell();
-      cell.text = (j).toString();
-      tableFields.add(new Field(row, cell, i, j));
-    }
-  }
-
-  const oneSec = const Duration(milliseconds: 1);
-  new Timer.periodic(oneSec, (Timer t) => doSomething());
+  const oneSec = const Duration(milliseconds: 10);
+  new Timer.periodic(oneSec, (Timer t) => moveBall());
 
 }
 
-void doSomething() {
-  for (int i = 0 ; i < tableFields.length ; i++) {
-    var rng = new Random();
-    Field field = tableFields[i];
-    field.getCell.text = rng.nextInt(10).toString();
-  }
+void moveBall() {
+  double x = fruit.x;
+  x+= x+1;
+  double y = (-x^2) + 0.5;
+  fruit.move(x, y);
+  container.update(fruit);
 }
