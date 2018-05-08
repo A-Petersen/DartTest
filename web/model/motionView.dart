900a1e798545c41cb78f591db19204a6d10c3cbc
n0ld <a.petersen@stud.fh-luebeck.de>
2018-05-08 11:09:10 +0200

    MVC - Erste Modeleintragungen

---
+++ web/model/motionView.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+class motionView {
+  /**
+   * Breite der gesamt View
+   */
+  int get width => window.innerWidth;
+
+  /**
+   * Höhe der gesamt View
+   */
+  int get hight => window.innerHeight;
+
+  double get center_X => this.width / 2;
+
+  
+}
