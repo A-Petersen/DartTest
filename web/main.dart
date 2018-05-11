09dec83f0dcd300cc77c1c3a78fd4b85ff1fef73
DanielFGA <DanielFGA>
2018-05-11 13:02:36 +0200

    Die Frucht bekommt Bewegeung endgültig abgeben Frank wird leben eingehaucht Der Controller controlliert alles nun Mehrere Objekte gleichzeitig, funktioniert aber noch nicht wirklich so schön

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,15 @@
 import 'Controller.dart';
+import 'dart:async';
 
 
 void main() {
 
   Controller controller = new Controller();
 
+  Timer timer = new Timer.periodic(new Duration(milliseconds: 5000), (Timer t) => controller.start(50, 100.0, -200.0, controller.newFruit(0.0, 295.0, 10.0)));
+  //controller.start(50, 100.0, -200.0, controller.newFruit(0.0, 295.0, 10.0));
+
 }
+
+
 
