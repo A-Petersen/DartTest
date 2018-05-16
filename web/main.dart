296173aff493b42addd0f6af3d3647a82d4533ae
DanielFGA <DanielFGA>
2018-05-16 23:22:47 +0200

    Die Frucht bekommt Bewegeung endgültig abgeben Frank wird leben eingehaucht Der Controller controlliert alles nun Mehrere Objekte gleichzeitig, funktioniert aber noch nicht wirklich so schön

--- web/main.dart
+++ web/main.dart
@@ -10,6 +10,9 @@ void main() {
   controller.newFruit(0.0, 0.0, 10.0, 20.0, 2.0);
   //Timer timer = new Timer.periodic(new Duration(milliseconds: 500000), (Timer t) => controller.newFruit(0.0, 295.0, 10.0));
   //controller.start(50, 100.0, -200.0, controller.newFruit(0.0, 295.0, 10.0));
+}
+
+void danielVersuchtSichAnEinemLevelKonzept() {
 
 }
 
