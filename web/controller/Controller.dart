cc2799c89c7b6da7a0d0d84e5a3ed97c162dae3f
DanielFGA <daniel.figia@stud.fh-luebeck.de>
2018-05-28 10:38:25 +0200

    aufregung

--- web/controller/Controller.dart
+++ web/controller/Controller.dart
@@ -15,23 +15,17 @@ class Controller {
   final Field field = new Field();
   Timer timerStart;
   Timer timerNewFruit;
-//  Figure frank;
   Game game;
-
-
+  Duration timeIntevall = new Duration(milliseconds: 50);
+  Duration throwIntevall = new Duration(milliseconds: 5000);
 
   Controller() {
     figureControll();
     game = new Game(field, this);
-    timerStart = new Timer.periodic(new Duration(milliseconds: 50), (Timer t) => start());
-    timerNewFruit = new Timer.periodic(new Duration(milliseconds: 2000), (Timer t) => checkFruits());
-//    frank = new Figure(0.0, 280.0, 100.0, 100.0, field);
+    timerStart = new Timer.periodic(timeIntevall, (Timer t) => start());
+    timerNewFruit = new Timer.periodic(throwIntevall, (Timer t) => checkFruits());
   }
 
-//  void movement(Fruit fruit) {
-//    fruit.move();
-//    field.updateFruit(fruit);
-//  }
 
   void checkFruits() {
     game.checkFruits();
@@ -44,30 +38,6 @@ class Controller {
    */
   void start() {
 
-//    for (int i = 0 ; i < game.fruits ; i++) {
-//      if (game.fruitsList[i].moving) {
-//        movement(game.fruitsList[i]);
-//        if ((game.fruitsList[i].y >= 260.0 && !frank.onDrum(game.fruitsList[i]))) {
-//          game.fruitsList[i].moving = false;
-//          removeFruitView(game.fruitsList[i--]);
-//          if (--game.attempts <= -100) {
-//            gameover();
-//            return;
-//          }
-//        }
-//        if (game.fruitsList[i].y > field.height - (frank.b * 0.75) && frank.onDrum(game.fruitsList[i])) {
-//          game.fruitsList[i].goingUp = true;
-//        }
-//
-//        if (game.fruitsList[i].left >= 480 && game.fruitsList[i].heaven >= 240) {
-//          game.fruitsList[i].moving = false;
-//          field.setScore(++game.score);
-//        }
-//
-//      } else {
-//        game.removeFruit(game.fruitsList[i--]);
-//      }
-//    }
 
     game.checkFruitState();
     game.figure.move();
--- web/main.dart
+++ web/main.dart
@@ -4,8 +4,10 @@ import 'dart:html';
 import 'model/Game.dart';
 import 'model/Level.dart';
 import 'dart:math';
+import 'package:pwa/client.dart' as pwa;
 
 void main() {
+  new pwa.Client();
   Controller controller = new Controller();
 }
 
