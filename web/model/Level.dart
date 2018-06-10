ea69f70948dd77fcc02b34de1a91ebd774f22f5f
DanielFGA <DanielFGA>
2018-06-10 19:20:46 +0200

    OrientationInfo hinzugefügt

--- web/model/Level.dart
+++ web/model/Level.dart
@@ -1,9 +1,13 @@
 class Level {
-
   int level = 1;
   int maxFruits = 3;
   int possibleFruits = 1;
   int possibleMovments = 0;
+
+  Level() {
+    new File('Levelkonzept')
+
+  }
 
   void reset() {
     level = 1;
